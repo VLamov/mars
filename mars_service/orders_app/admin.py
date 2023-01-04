8c8bbb4bd82c6e118bdcb74d701f87b87fcde671
VLamov <vlamov@mail.ru>
2023-01-04 14:44:12 +0700

    Получились модели

--- mars_service/orders_app/admin.py
+++ mars_service/orders_app/admin.py
@@ -1,20 +1,24 @@
-from django.contrib import admin
-
 # Register your models here.
 from django.contrib import admin
+
 from .models import Order, Device, Customer, DeviceInField
+
 
 class DeviceAdmin(admin.ModelAdmin):
     list_display = ('id', "manufacturer", 'model')
 
+
 class CustomerAdmin(admin.ModelAdmin):
     list_display = ('id', 'customer_name', 'customer_address', 'customer_city')
+
 
 class DevicesInFieldAdmin(admin.ModelAdmin):
     list_display = ('id', 'serial_number', 'customer_id', 'analyzer_id', 'owner_status')
 
+
 class OrderAdmin(admin.ModelAdmin):
     list_display = ('id', 'device', 'customer_id', 'order_description', 'created_dt', 'last_updated_dt', 'order_status')
+
 
 admin.site.register(Device, DeviceAdmin)
 admin.site.register(Customer, CustomerAdmin)
